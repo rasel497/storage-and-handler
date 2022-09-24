0478ab017b0912c8e5f53f8a08b828f68e33985d
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-24 14:33:26 +0600

    create button Event handler with parameters

--- src/componants/Cosmitic/Cosmitic.js
+++ src/componants/Cosmitic/Cosmitic.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import './Cosmitic.css'
 const Cosmitic = (props) => {
     const { name, price, id } = props.cosmitic;
 
+    const addToCart = (id) => {
+        console.log('add items', id)
+    }
+    // const addToCartWithPara = () => addToCart(id
     return (
-        <div>
+        <div className='product'>
             <h2>Buy this: {name}</h2>
-            <p>Only for: {price}</p>
+            <p>Only for: ${price}</p>
             <p>This id: {id}</p>
+
+            <button onClick={() => addToCart(id)}>Add to cart</button>
         </div>
     );
 };
