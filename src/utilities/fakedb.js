2fea8e43044b591fa65d8a2ab47fb354c319279b
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-24 17:15:46 +0600

    store multiple data in an object with local storage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,17 +1,26 @@
 // use local storage to manage cart data, add, remove, update
 const addToDb = id => {
-    const quantity = localStorage.getItem(id);
-    if (quantity) {
-        console.log('alreday exists');
-        // for added multiple item
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity)
+    let shoppingCart = {};
+
+    //get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        shoppingCart = JSON.parse(storedCart);
     }
     else {
-        console.log('new item added')
-        localStorage.setItem(id, 1);
+        shoppingCart = {};
     }
-
+    //add quantity
+    const quantity = shoppingCart[id];
+    if (quantity) {
+        // for buy multiple item
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else {
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
     // localStorage.setItem(id, 1); // initial check
 }
 
