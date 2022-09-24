15463037417cc73095e3708c14526adf0805dbd0
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-24 18:19:23 +0600

    remove single shopping cart And delete full shopping cart from db local storage

--- src/componants/Cosmitic/Cosmitic.js
+++ src/componants/Cosmitic/Cosmitic.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, deleteShoppingCart, deleteShoppingCartFromBd, removeFromDb } from '../../utilities/fakedb';
 import './Cosmitic.css'
 
 const Cosmitic = (props) => {
     const { name, price, id } = props.cosmitic;
 
+    //add one item in button event handler
     const addToCart = (id) => {
         // console.log('add items', id); //only item add console
         // sessionStorage.setItem(id, 1); // item set local stroge in this line
         // ekhon amra alada vabe localStorage mange koerer jonno fakedb.js file create korlm
         addToDb(id);
     }
-    // const addToCartWithPara = () => addToCart(id
+
+    //remove one item in button event handler
+    const removeFromCart = id => {
+        // console.log('removing', id); initial test
+        removeFromDb(id); // link from fakeDb.js file
+    }
+
+    //delete full shopping-cart in button event handler
+    const deleteShoppingCart = () => {
+        deleteShoppingCartFromBd(id)
+    }
+
     return (
         <div className='product'>
             <h2>Buy this: {name}</h2>
@@ -19,6 +31,8 @@ const Cosmitic = (props) => {
             <p>This id: {id}</p>
 
             <button onClick={() => addToCart(id)}>Add to cart</button>
+            <button onClick={() => removeFromCart(id)}>Remove</button>
+            <button onClick={() => deleteShoppingCart(id)}>Delete All</button>
         </div>
     );
 };
--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,15 +1,18 @@
 // use local storage to manage cart data, add, remove, update
+//01- addToDb
 const addToDb = id => {
     let shoppingCart = {};
 
-    //get the shopping cart
+    //get the shopping cart from local storage
     const storedCart = localStorage.getItem('shopping-cart');
     if (storedCart) {
         shoppingCart = JSON.parse(storedCart);
     }
     else {
+        // default Ei line na dile hy cz, 3no line defalut dewa ase.
         shoppingCart = {};
     }
+
     //add quantity
     const quantity = shoppingCart[id];
     if (quantity) {
@@ -24,4 +27,22 @@ const addToDb = id => {
     // localStorage.setItem(id, 1); // initial check
 }
 
-export { addToDb }
+//02- remove from db storage ja amra appilication dekhi
+const removeFromDb = id => {
+    const storedCart = localStorage.getItem('shopping-cart');
+    if (storedCart) {
+        const shoppingCart = JSON.parse(storedCart);
+        if (id in shoppingCart) {
+            delete shoppingCart[id];
+            localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+        }
+    }
+}
+
+// optional amii chaile pura shopping cart taii delete kore dite pari
+const deleteShoppingCartFromBd = () => {
+    localStorage.removeItem('shopping-cart');
+}
+
+
+export { addToDb, removeFromDb, deleteShoppingCartFromBd }
